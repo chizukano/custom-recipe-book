edb58084f7b7202fec3cf6af4bd8355a817b7a8d
chizukano <chizu0303@gmail.com>
2022-03-29 19:11:53 -0600

    rails setup

---
+++ db/migrate/20220330004931_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.text :description
+      t.text :ingredients
+      t.integer :portion
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "creating user"
+user = User.create!(
+  username: "chizu",
+  email: "chizu@user.com",
+  password: "123456"
+)
+
+puts "done"
+
+puts "creating categories"
+
+category1 = Category.create!(
+  name: "meals"
+)
+
+category2 = Category.create!(
+  name: "desserts"
+)
+
+puts "done"
+
+puts "creating recipes"
+
+Recipe.create!(
+  name: "Pound Cake",
+  description: "Add each ingredients (from wet ingredients first) into the bowl and stir.",
+  ingredients: "Eggs, cups of Milk, Flour",
+  portion: "2, 1, 200",
+  user: user,
+  category: category2
+)
+
+Recipe.create!(
+  name: "Meatball",
+  description: "Add each ingredients (from wet ingredients first) into the bowl and stir.",
+  ingredients: "Eggs, cups of Milk, Flour, Ground Beef",
+  portion: "2, 1, 100, 300",
+  user: user,
+  category: category1
+)
+
+puts "done"
