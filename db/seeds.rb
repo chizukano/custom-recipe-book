8debd59bf7d0d4fcc6219d8a65993ff73f1c4266
chizukano <chizu0303@gmail.com>
2022-03-30 17:51:29 -0600

    Added photo on categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "creating user"
 user = User.create!(
   username: "chizu",
-  email: "chizu@user.com",
+  email: "chizu@users.com",
   password: "123456"
 )
 
@@ -12,16 +12,16 @@ puts "creating categories"
 category1 = Category.create!(
   name: "meals"
 )
-# file = File.open("db/support/meal.jpg")
-# category1.photo.attach(io: file, filename: "meal.jpg", content_type: 'image/jpg')
-# category1.save!
+file = File.open("db/support/meal.jpg")
+category1.photo.attach(io: file, filename: "meal.jpg", content_type: 'image/jpg')
+category1.save!
 
 category2 = Category.create!(
   name: "desserts"
 )
-# file2 = File.open("db/support/meal.jpg")
-# category2.photo.attach(io: file2, filename: "dessert.jpg", content_type: 'image/jpg')
-# category2.save!
+file2 = File.open("db/support/dessert.jpg")
+category2.photo.attach(io: file2, filename: "dessert.jpg", content_type: 'image/jpg')
+category2.save!
 
 puts "done"
 
@@ -35,9 +35,9 @@ recipe1 = Recipe.create!(
   user: user,
   category: category2
 )
-# file3 = File.open("db/support/poundcake.jpg")
-# recipe1.photo.attach(io: file3, filename: "poundcake.jpg", content_type: 'image/jpg')
-# recipe1.save!
+file3 = File.open("db/support/poundcake.jpg")
+recipe1.photo.attach(io: file3, filename: "poundcake.jpg", content_type: 'image/jpg')
+recipe1.save!
 
 recipe2 = Recipe.create!(
   name: "Meatball",
@@ -47,8 +47,8 @@ recipe2 = Recipe.create!(
   user: user,
   category: category1
 )
-# file3 = File.open("db/support/meatball.jpg")
-# recipe2.photo.attach(io: file4, filename: "meatball.jpg", content_type: 'image/jpg')
-# recipe2.save!
+file4 = File.open("db/support/meatball.jpg")
+recipe2.photo.attach(io: file4, filename: "meatball.jpg", content_type: 'image/jpg')
+recipe2.save!
 
 puts "done"
