6b2535ee5f76d2d7fa2d0fea30ee1304b6bd7f7d
chizukano <chizu0303@gmail.com>
2022-03-31 10:55:19 -0600

    create recipes index only for its category

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(category_id: params[:category_id])
     @category = Category.find(params[:category_id])
   end
 end
