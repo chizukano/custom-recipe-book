9567bd802af97625a53308822b194aefe0920899
chizukano <chizu0303@gmail.com>
2022-04-01 14:39:42 -0600

    added links and edit, edited navbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+
+    if @category.save
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def category_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :categories, only: [:index, :new, :create] do
+  resources :categories, only: [:index, :new, :create, :edit, :update] do
     resources :recipes, only: [:show, :index]
   end
 end
