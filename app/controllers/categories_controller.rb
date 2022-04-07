2609c331df75dfba0116c6775fe7f77bfaea18b9
chizukano <chizu0303@gmail.com>
2022-04-07 09:35:44 -0500

    Changed authentication for general pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
     @categories = policy_scope(Category).order(created_at: :desc)
   end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[show index]
+
   def index
     @category = Category.find(params[:category_id])
     @recipes = policy_scope(Recipe).where(category_id: params[:category_id])
