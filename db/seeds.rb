d71e8f195a443173e30895aef1a4f105d13a21ff
chizukano <chizu0303@gmail.com>
2022-04-06 19:12:58 -0500

    Added ingredients & r.ingredients to show

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,63 +32,132 @@ category2.save!
 puts "done"
 
 puts "creating ingredients"
-ingredient1 = Ingredient.create!(
-  name: "wheat flour"
-)
 
-ingredient2 = Ingredient.create!(
-  name: "butter"
-)
+ingredients1 = [
+  "ground beef",
+  "garlic cloves",
+  "ginger slices",
+  "soy sauce",
+  "butter",
+  "salt",
+  "pepper",
+  "cabbage",
+  "potatoes",
+  "carrots",
+  "corn starch",
+  "lemon"
+]
+
+quantities1 = [
+  200,
+  2,
+  5,
+  5,
+  30,
+  0.25,
+  0.25,
+  0.5,
+  3,
+  3,
+  1.5,
+  0.5
+]
+
+units1 = [
+  "g",
+  "",
+  "",
+  "tsp",
+  "g",
+  "tbsp",
+  "tbsp",
+  "",
+  "",
+  "",
+  "tbsp",
+  ""
+]
+
+ingredients2 = [
+  "wheat flour", "sugar", "eggs", "bananas", "chocolate", "water", "corn starch",
+  "butter", "apples", "brown sugar", "coconut oil"
+]
+
+quantities2 = [
+  200, 90, 3, 2, 100, 0.75, 100, 2.5, 3, 30, 2
+]
+
+units2 = [
+  "g", "g", "", "", "g", "cup", "g", "tsp", "", "g", "tbsp"
+]
+
+meal_ingredients = []
+dessert_ingredients = []
+
+ingredients1.each do |ingredient|
+  meal_ingredients << Ingredient.create!(
+    name: ingredient
+  )
+end
+
+ingredients2.each do |ingredient|
+  dessert_ingredients << Ingredient.create!(
+    name: ingredient
+  )
+end
 
 puts "done"
 
 puts "creating recipes"
 
-recipe1 = Recipe.create!(
-  name: "Pound Cake",
-  description: "Recipe from Cooking.com",
-  portion: "2",
-  user: user,
-  category: category2
-)
-file3 = File.open("db/support/poundcake.jpg")
-recipe1.photo.attach(io: file3, filename: "poundcake.jpg", content_type: 'image/jpg')
-recipe1.save!
 
-recipe2 = Recipe.create!(
+recipe1 = Recipe.create!(
   name: "Meatball",
   description: "Recipe from my grandma",
   portion: "5",
   user: user,
   category: category1
 )
-file4 = File.open("db/support/meatball.jpg")
-recipe2.photo.attach(io: file4, filename: "meatball.jpg", content_type: 'image/jpg')
+file3 = File.open("db/support/meatball.jpg")
+recipe1.photo.attach(io: file3, filename: "meatball.jpg", content_type: 'image/jpg')
+recipe1.save!
+
+recipe2 = Recipe.create!(
+  name: "Pound Cake",
+  description: "Recipe from Cooking.com",
+  portion: "2",
+  user: user,
+  category: category2
+)
+file4 = File.open("db/support/poundcake.jpg")
+recipe2.photo.attach(io: file4, filename: "poundcake.jpg", content_type: 'image/jpg')
 recipe2.save!
 
 puts "done"
 
 puts "creating recipe ingredients"
 
-RecipeIngredient.create!(
-  quantity: 300,
-  unit: "grams",
-  recipe: recipe1,
-  ingredient: ingredient1
-)
+recipe_ingredients1 = []
+index = 0
 
-RecipeIngredient.create!(
-  quantity: 90,
-  unit: "grams",
-  recipe: recipe1,
-  ingredient: ingredient2
-)
+meal_ingredients.each do |ingredient|
+  RecipeIngredient.create!(
+    quantity: quantities1[(index += 1) % quantities1.size],
+    unit: units1[(index += 1) % units1.size],
+    recipe: recipe1,
+    ingredient: ingredient
+  )
+end
 
-RecipeIngredient.create!(
-  quantity: 50,
-  unit: "grams",
-  recipe: recipe2,
-  ingredient: ingredient2
-)
+recipe_ingredients2 = []
+
+dessert_ingredients.each do |ingredient|
+  RecipeIngredient.create!(
+    quantity: quantities2[(index += 1) % quantities2.size],
+    unit: units2[(index += 1) % units2.size],
+    recipe: recipe2,
+    ingredient: ingredient
+  )
+end
 
 puts "done"
